efab363e4d7adbd7efaa99017dde832eb446c0fd
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-16 20:10:59 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-//import Request from 'axios-react';
 
 import send from "./assets/send.svg";
 import user from "./assets/user.png";
