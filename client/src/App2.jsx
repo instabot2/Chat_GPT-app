49a05c1308c4aef3e051d555d5d3b2adb8b90b4d
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-16 15:14:38 +0800

    Create

---
+++ client/src/App2.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:8080/", {
+        input: input,
+      });
+
+      setResponse(res.data.bot);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={input} onChange={handleInputChange} />
+        <button type="submit">Submit</button>
+      </form>
+
+      {response && (
+        <div>
+          <p>Bot:</p>
+          <p>{response}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
