ad6090f04c40e423926dfb2a5743e1b2bdc12899
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-17 20:05:53 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,46 +1,14 @@
-//import React from "react";
+import React from "react";
 //import ReactDOM from "react-dom/client";
 //import ReactDOM from "react-dom";
 //import App from "./App";
 //import "./index.css";
 //ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
+import './index.css';
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
-import App from "./App";
-import "./index.css";
-
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    console.error(error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // Fallback UI
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return this.props.children;
-  }
-}
-
-ReactDOM.render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>,
-  document.getElementById("root")
-);
+  
