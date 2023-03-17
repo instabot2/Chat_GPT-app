80007fa03abba02b2d511fb769be9ba7205aff1c
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-17 22:17:32 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-//ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-ReactDOM.render(<App />, document.getElementById('root'));                                                            
-                                                   
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+   
