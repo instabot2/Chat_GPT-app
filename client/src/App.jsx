f392fa9655fdbbe950c23f0dfa527adb180ab158
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-17 20:38:39 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,19 @@
-import React, { useState } from "react";
-import axios from "axios";
-//import "./index.css";
+import { useState } from 'react';
 
-function App() {
-  const [input, setInput] = useState("");
-  const [response, setResponse] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    try {
-      const res = await axios.post("https://chatgpt-ai-83yl.onrender.com", { input });
-      setResponse(res.data.bot);
-      setInput("");
-    } catch (error) {
-      console.error(error);
-      setResponse("An error occurred. Please try again later.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
+export default function App() {
   return (
-    <div className="chatGPT-app">
-      <h1>ChatGPT AI</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter your message..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Sending..." : "Send"}
-        </button>
-      </form>
-      <div className="response">{response}</div>
-    </div>
+    <>
+      <h1>Hello, world!</h1>
+      <Counter />
+    </>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      You clicked me {count} times
+    </button>
+  );
+}
