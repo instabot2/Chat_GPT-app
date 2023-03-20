dfe864998a8ed59e7c58d91bc57a1dcfb7216b83
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-20 10:36:16 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,8 +17,7 @@ function App() {
 
   const fetchBotResponse = async () => {
     const { data } = await axios.post(
-      "https://mychatgpt-app.onrender.com",
-      //"https://chatgpt-ai-lujs.onrender.com",
+      "https://chatgpt-ai-lujs.onrender.com",
       { input },
       {
         headers: {
