22d445cc1227980a7b60623a5cb8b4ef4a443dfe
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-23 13:44:10 +0800

    Create popup.js

---
+++ client/popup.js
@@ -0,0 +1,16 @@
+const imageContainer = document.getElementById('image-container');
+const imageUrl = '/public/rotater.gif';
+const displayTime = 5000; // display time in milliseconds
+
+// create the image element and set its properties
+const image = document.createElement('img');
+image.src = imageUrl;
+image.style.width = '100%';
+
+// add the image to the container
+imageContainer.appendChild(image);
+
+// set a timeout to remove the image after the display time has passed
+setTimeout(() => {
+  imageContainer.removeChild(image);
+}, displayTime);
