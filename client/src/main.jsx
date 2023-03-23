08c91069c272457b115463fdbeed40093fef7c64
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-23 14:28:32 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,41 @@ const gifUrl = "/rotater.gif";
 
 //ReactDOM.createRoot(document.getElementById("root")).render(<App />);
                                                                                                         
-const handleClick = (event) => {
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const gifUrl = "/rotater.gif";
+
+const handleImageClick = (event, setImageVisible) => {
   event.target.style.display = "none";
+  setImageVisible(false);
 };
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-    <img src={gifUrl} alt="Rotater GIF" style={{ cursor: "pointer", width: "50%" }} onClick={handleClick} />
-    <App />
-  </div>
-);
+const Root = () => {
+  const [isImageVisible, setImageVisible] = useState(true);
+
+  return (
+    <div style={{ position: "relative", width: "50%", height: "100vh" }}>
+      {isImageVisible && (
+        <img
+          src={gifUrl}
+          alt="Rotater GIF"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            cursor: "pointer",
+            zIndex: 999,
+          }}
+          onClick={(event) => handleImageClick(event, setImageVisible)}
+        />
+      )}
+      <App />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
