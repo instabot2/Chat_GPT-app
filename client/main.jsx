34537962ea95fb5cc6f8debcfe6aefb8a6c578cb
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-23 12:41:00 +0800

    Create main.jsx

---
+++ client/main.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Popup from './components/Popup';
+
+const App = () => {
+  const [popupVisible, setPopupVisible] = useState(true);
+
+  const handlePopupClose = () => {
+    setPopupVisible(false);
+  };
+
+  return (
+    <>
+      {popupVisible && (
+        <Popup onClose={handlePopupClose}>
+          <img
+            src="path/to/gif/image.gif"
+            alt="Popup GIF"
+            onClick={handlePopupClose}
+          />
+        </Popup>
+      )}
+
+      {/* Rest of your app code here */}
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
