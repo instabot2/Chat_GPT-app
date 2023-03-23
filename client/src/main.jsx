7b9ebf190ce9b86898c2bd131c0db1b5687a66df
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-23 14:20:49 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,4 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const gifUrl = "/rotater.gif";
+
+//ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+                                                          
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <img src={gifUrl} alt="Rotater GIF" />
+    <App />
+  </div>
+);
+                                                      
