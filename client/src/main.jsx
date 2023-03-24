c7d8fe5133ed4e23c49b6064830717e3d472d573
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-24 12:45:45 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -21,7 +21,7 @@ const Root = () => {
 
   return (
     //<div style={{ position: "relative", width: "100vw", height: "100vh" }}>
-    <div style={{ position: "relative", width: "80%", height: "80%" }}>
+    <div style={{ position: "relative", width: "100%", height: "100%" }}>
       {isImageVisible && (
         <img
           src={gifUrl}
