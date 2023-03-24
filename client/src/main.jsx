78997917a4904f773e0462d45bd8752fc4a387e1
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-24 16:35:06 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -32,7 +32,14 @@ const Root = () => {
   };
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100%" }}>
+    <div style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",}}>
+
       {isImageVisible && !isError && (
         <>
           {isLoading && (
