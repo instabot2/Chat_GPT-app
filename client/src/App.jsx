f3b6f7da89e80e4653fb95cbc14b8833df81ed29
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-25 10:32:38 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -147,7 +147,8 @@ function App() {
           placeholder="Ask anything!"
           onChange={(e) => setInput(e.target.value)}
           onKeyUp={onKeyUp}
-        />
+          style={{ overflow: 'auto' }} // add this line to add a scrollbar
+        />    
         <div className="send-button" onClick={onSubmit}>
           <img src={send} />
         </div>
