c436f4237cbf83722931a75f8aabb66603fa186f
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-26 19:45:52 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -215,7 +215,7 @@ function App() {
         />
 
         <div className="send-button" onClick={onSubmit}>
-          <img src={send} />&nbsp;&nbsp;&nbsp;
+          <img src={send} />&nbsp;&nbsp;
           <a href="#" onClick={handleLogout}><img src={trash} alt="trash" /></a>
         </div>
 
