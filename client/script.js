31a35c6656387a02d1fffe95c0c248dbc6cdca09
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-27 18:46:07 +0800

    Delete script.js

--- client/script.js
+++
@@ -1,9 +0,0 @@
-const imageUrl = "rotater.gif";
-const displayTime = 5000;
-
-const image = document.getElementById("image");
-image.src = imageUrl;
-
-setTimeout(() => {
-  image.src = "";
-}, displayTime);
