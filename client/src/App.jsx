86e20791252097c7db33768111faadb5d777ecfd
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-28 14:32:40 +0800

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatHistory from './ChatHistory';
+
+function App() {
+  return (
+    <div>
+      <h1>My Chat App</h1>
+      <ChatHistory />
+    </div>
+  );
+}
+
+export default App;
