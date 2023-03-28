27e1a4be7555045d6a004dfe84f4262794badb92
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-28 18:00:28 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import trash from "./assets/trash.png";
 import user from "./assets/user.png";
 import bot from "./assets/bot.png";
 import loadingIcon from "./assets/loader.svg";
-
 //port React from 'react';
 
 function App() {
@@ -31,29 +30,27 @@ function App() {
     layout.scrollTop = layout.scrollHeight;
   }, [posts]);
 
-  
   // if error response data, try fixing the API key at server render
   const fetchBotResponse = async (input) => {
     try {
-      const response = await fetch("https://chatgpt-ai-83yl.onrender.com", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ input }),
-      });
+      const response = await axios.post(
+        "https://chatgpt-ai-83yl.onrender.com",
+        { input },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-      const data = await response.json();
-      
-      if (!data) {
+      if (!response.data) {
         throw new Error("No response data received from bot.");
       }
 
-      return data;
+      return response.data;
     } catch (error) {
       console.error("Error fetching bot response: ", error);
       throw new Error("Could not fetch bot response.");
-      return null;
     }
   };
 
@@ -72,33 +69,35 @@ function App() {
         updatePosts("Error fetching bot response.", true);
       });
   };
-  
+
   const autoTypingBotResponse = (text) => {
     let index = 0;
-    let newPosts = [...posts];
     let interval = setInterval(() => {
       if (index < text.length) {
-        let lastItem = newPosts.length > 0 ? newPosts.pop() : null;
-        if (lastItem && lastItem.type !== "bot") {
-          newPosts.push({
-            type: "bot",
-            post: text.charAt(index - 1),
-          });
-        } else if (lastItem) {
-          newPosts.push({
-            type: "bot",
-            post: lastItem.post + text.charAt(index - 1),
-          });
-        } else {
-          newPosts.push({
-            type: "bot",
-            post: text.charAt(index - 1),
-          });
-        }
+        setPosts((prevState) => {
+          let lastItem = prevState.length > 0 ? prevState.pop() : null;
+          if (lastItem && lastItem.type !== "bot") {
+            prevState.push({
+              type: "bot",
+              post: text.charAt(index - 1),
+            });
+          } else if (lastItem) {
+            prevState.push({
+              type: "bot",
+              post: lastItem.post + text.charAt(index - 1),
+            });
+          } else {
+            prevState.push({
+              type: "bot",
+              post: text.charAt(index - 1),
+            });
+          }
+          historyRef.current = [...prevState];
+          return [...prevState];
+        });
         index++;
       } else {
         clearInterval(interval);
-        setPosts(newPosts);
       }
     }, 20);
   };
@@ -118,14 +117,13 @@ function App() {
       });
     }
   };
-  
-  
+
   const onKeyUp = (e) => {
     if (e.key === "Enter" || e.which === 13) {
       onSubmit();
     }
-  }; 
-  
+  };
+
   const handleUndo = () => {
     const prevHistory = historyRef.current;
     if (prevHistory.length > 0) {
@@ -137,41 +135,6 @@ function App() {
       });
       setInput(lastItem.post);
     }
-  };
-
-  const scrollToBottom = () => {
-    const layout = document.querySelector(".layout");
-    layout.scrollTop = layout.scrollHeight;
-  };
-
-  
-  const clearCacheAndHistory = () => {
-    const confirmed = confirm("Clear cache and history?");
-    if (confirmed) {
-      window.localStorage.setItem("imageDisplayed", "false");
-      window.location.reload(true);
-      window.localStorage.clear();
-      window.sessionStorage.clear();
-      window.history.replaceState(null, null, window.location.href);
-    }
-  };
-  window.addEventListener("load", () => {
-    const imageDisplayed = window.localStorage.getItem("imageDisplayed");
-    if (imageDisplayed !== "true") {
-      document.getElementById("overlay").style.display = "block";
-      window.localStorage.setItem("imageDisplayed", "true");
-    } else {
-      document.getElementById("overlay").style.display = "none";
-    }
-  });
-  document.getElementById("overlay").addEventListener("click", () => {
-    document.getElementById("overlay").style.display = "none";
-    window.localStorage.setItem("imageDisplayed", "false");
-  });
-  const handleLogout = () => {
-    // Perform any necessary logout logic
-    // Clear cache and history
-    clearCacheAndHistory();
   };
   
   return (
