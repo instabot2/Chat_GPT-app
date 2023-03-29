62a3adc88521b2c3c3ce5d1a56735756fded527e
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-29 15:59:00 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -234,19 +234,10 @@ function App() {
                 ) : (
                   <div className="post">
                   
+                  <img src={copyImage} alt="Copy" />
                   <img
                     src={copyImage}
                     alt="Copy"
-                    style={{
-                      width: "20px",
-                      height: "20px",
-                      cursor: "pointer",
-                      position: "absolute",
-                      left: "50%",
-                      bottom: "-20px",
-                      transform: "translateX(-50%)",
-                      pointerEvents: "auto", // Add this line
-                    }}
                     onClick={() => copyToClipboard(post.post)}
                   />
        
