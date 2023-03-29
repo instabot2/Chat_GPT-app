aa60fd1288abb248d0a469483de01be7df015c0d
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-29 16:05:16 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -241,7 +241,7 @@ function App() {
                       width: "20px",
                       height: "20px",
                       cursor: "pointer",
-                      position: "absolute",
+                      //position: "absolute",
                       left: "50%",
                       bottom: "-20px",
                       //transform: "translateX(-50%)",
