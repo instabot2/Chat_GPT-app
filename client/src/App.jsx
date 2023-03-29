669ed9eecc0c6f1bede970b1fdcf36ee62460040
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-29 11:19:21 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -238,10 +238,6 @@ function App() {
                   <button className="copy-btn" onClick={() => copyToClipboard(post.post)}>
                     <i className="fas fa-copy"></i>
                   </button>
-                  
-                  <button className="copy-btn" onClick={() => copyToClipboard(post.post)}>
-                    <img src="../assets/copy.png" alt="" />
-                  </button>
 
                 </div>
               )}
